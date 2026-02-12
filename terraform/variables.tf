67276fe80cc0ada502992b90e6af803eaf064156
larsb-dev <github@worldagainst.me>
2026-02-12 10:41:39 +0100

    feat: add terraform apply workflow step to rebuild container automatically

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,9 @@ variable "location" {
   type    = string
   default = "Switzerland North"
 }
+
+variable "image_tag" {
+  description = "Docker image tag"
+  type        = string
+  default     = "latest"
+}
